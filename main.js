dcc3d9a0141d95cd01b55ff03bcadc7f5dfe2c35
PavelGoblin <pvlbrhm@gmail.com>
2024-10-16 10:47:58 +0600

    part 1 dONe

--- main.js
+++ main.js
@@ -8,3 +8,20 @@ const resetItems =() => {
     item => item.classList.remove("active")
   )
 };
+
+const handleClick =(element,index) => {
+  resetItems();
+  element.classList.add("active");
+  localStorage.setItem("accordationIndex",index)
+};
+
+resetItems();
+
+const activeItem =
+items[localStorage.getItem("accordationIndex")];
+
+if (activeItem){
+  activeItem.classList.add("active");
+} else{
+  items[0].classList.add("active");
+}
